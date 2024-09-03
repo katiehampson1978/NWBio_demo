ffcf6d84e84fd9ce34c22a0fe1b161c2aaeb1583
Katie Hampson <katiehampson@gmail.com>
2024-09-03 23:00:23 +0100

    edits to
    - clean up data file
    - make figures
    - save outputs

---
+++ Github_demo_Katie.R
@@ -0,0 +1,6 @@
+#' _Github Demo_ 
+#' Read in the data provided 
+#' Plot a barchart to show the most popular lunch destination
+#' Clean the data to make the plot better
+#' Create your own version in a new repo for your own university/ site
+#' 
